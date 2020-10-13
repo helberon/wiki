c8f39ba632cf038672a4e3b8ac43ec5a13728faf
helberon <helberon@gmail.com>
2020-10-13 17:21:11 +0300

    cross-browser testing approach (Chrome, FireFox, Edge)

---
+++ src/main/java/driverFactory/DriverManager.java
@@ -0,0 +1,41 @@
+package driverFactory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverManager {
+    private static WebDriver driver;
+    private static String driverName = System.getProperty("driver") == null ? "chrome" : System.getProperty("driver");
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            switch (driverName) {
+                case ("chrome"):
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case ("firefox"):
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case ("edge"):
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+            }
+            driver.manage().deleteAllCookies();
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        driver.close();
+        if (driver != null) {
+            driver = null;
+        }
+    }
+}
